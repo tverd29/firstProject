4404138afa2c32e24a3fa09b03bf7d723d8a47c3
DESKTOP-P8T69TK\tverd <tverd29@gmail.com>
2019-03-02 02:43:41 +0300

    working at language

--- Akk/main.cpp
+++ Akk/main.cpp
@@ -1,9 +1,28 @@
 #include <QApplication>
+#include <QDir>
+#include <QSettings>
+#include <QTranslator>
 
 #include "Include\akkwindow.h"
 
 int main(int argc, char * argv[]) {
     QApplication a(argc, argv);
+
+    QSettings settings("settings_conf", QSettings::IniFormat);
+    settings.beginGroup("main_settings");
+    QString lang = settings.value("language", "").toString();
+    //    settings.setValue("language", "ru_RU");
+    settings.endGroup();
+
+    QTranslator translator;
+    if (lang.isEmpty()) {
+        lang = QLocale::system().name();
+    }
+    QString z = QDir::currentPath();
+    QString y = "V:/Git/First/Akk";
+    if (translator.load("akk_" + lang, y)) {
+        qApp->installTranslator(&translator);
+    }
     AkkWindow w;
     w.show();
 
